32801f04a290fc45c2863fba7345e33e9214df2c
Marios Gavanas <mgavanas@amcstudent.edu.gr>
2017-05-08 16:02:35 +0300

    fixes, updates and additions

--- app/src/main/java/com/sdmd/mgava/mypetsapp/PetDBSchemaHelper.java
+++ app/src/main/java/com/sdmd/mgava/mypetsapp/PetDBSchemaHelper.java
@@ -7,7 +7,7 @@ import android.database.sqlite.SQLiteOpenHelper;
 public class PetDBSchemaHelper extends SQLiteOpenHelper {
 
     private static final int DATABASE_VERSION = 1;
-    private static final String DATABASE_NAME = "Pet.db";
+    private static final String DATABASE_NAME = "PetInfo.db";
 
     public PetDBSchemaHelper(Context context) {
         super(context, DATABASE_NAME, null, DATABASE_VERSION );
